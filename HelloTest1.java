7d0b6ccd9af24755786d003fd023093825f92e76
0482381 <54283920+0482381@users.noreply.github.com>
2019-10-03 01:20:23 -0400

    Update of HelloTest1.java
    I added an update on line 3 and modified the comments on line 9.

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
  * Created on 8/3/18 by AJT
+ * Updated on 3 Oct 19 by KSH
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first Pull Request!
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
